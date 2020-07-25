7d5b3f606beb74b76c7f70aa33178671c3198454
1700016025 <dian1700016025@webmail.uad.ac.id>
2020-07-24 19:55:00 -1200

    first

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+      <div class="ui fixed inverted menu vue-color">
+        <div class="ui container">
+          <a href="#" class="header item">Dokter</a>
+        </div>
+     </div>
+
+     <div class="ui main container">
+       <MyForm />
+     </div>
+  </div>
+</template>
+
+<script>
+import MyForm from "./components/MyFrom";
+
+export default {
+  name: "App",
+  components: {
+    MyForm    
+  }
+}
+</script>
+
+<style>
+.vue-color {
+  background: #41b883 !important;
+}
+
+.main.container {
+  margin-top: 60px;
+}
+
+.submit-button {
+  margin-top: 24px !important;
+  float: right;
+}
+</style>
---
+++ src/components/MyFrom.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="my-form">
+      <form-class="ui form">
+        <div class="fields">
+        <div class="four wide field">
+            <label>name</label>
+            <input type="text" name="name" placeholder="name" />
+        </div>
+
+        <div class="six wide field">
+            <label>keahlian</label>
+            <input type="text" name="keahlian" placeholder="keahlian" />
+        </div>
+
+        <div class="two wide field">
+            <button class="ui primary button submit-button">Save</button>
+
+        </div>
+    </div>
+      </form>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "MyForm"
+};
+</script>
+
+<style scoped>
+
+</style>
